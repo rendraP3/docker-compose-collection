7ca8485867d953e32bd5f6d26799ecfca6f668c6
Rendra Prasetia <rendra.prasetia@bankmandiri.co.id>
2022-01-07 02:39:17 +0700

    add some helper scripts

---
+++ gitea/helpers/installer.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+
+# SSH Container Passthrough
+sudo mkdir -p /app/gitea
+sudo cp helpers/cp/gitea /app/gitea/gitea
+
+# Create git user, make its an owner of /var/lib/gitea, and log in
+sudo adduser --gecos ""  --disabled-password --home /var/lib/gitea/git git
+sudo chown git:git /var/lib/gitea
+
+USER_PASSWORD="$(pwgen -s 20 -y)"
+echo "git user password is \e[34m\"${USER_PASSWORD}\"\e[0m. \e[91mDON'T FORGET TO SAVE IT!\e[0m"
+sudo usermod --password "$(openssl passwd -1 "${USER_PASSWORD}")" git
+
+# Create SSH key pair for git user
+sudo -u git ssh-keygen -t rsa -b 4096 -C "Gitea Host Key" -N '' -f "/var/lib/gitea/git/.ssh/id_rsa"
+
+# Add host pub key to authorized_keys that will be shared between
+# server container and the host git user
+sudo -u git touch /var/lib/gitea/git/.ssh/authorized_keys
+sudo -u git chmod 644 /var/lib/gitea/git/.ssh/authorized_keys
+sudo -u git sh -c \
+"echo \"no-port-forwarding,no-X11-forwarding,no-agent-forwarding,no-pty $(sudo -u git cat /var/lib/gitea/git/.ssh/id_rsa.pub)\" >> /var/lib/gitea/git/.ssh/authorized_keys"
---
+++ gitea/helpers/iptables.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env sh
+# TL;DR docker bypasses ufw firewall, so any rules you will apply for 80, 443
+# ports simply won't work and they will be seen by the world
+# See this to learn more:
+# https://www.mkubaczyk.com/2017/09/05/force-docker-not-bypass-ufw-rules-ubuntu-16-04/
+
+# Docker shouldn't update iptables
+sudo sh -c 'cat > /etc/docker/daemon.json <<EOF
+{"iptables": false}
+EOF'
+
+# Set DEFAULT_FORWARD_POLICY to ACCEPT
+sudo sed -i -e 's/DEFAULT_FORWARD_POLICY="DROP"/DEFAULT_FORWARD_POLICY="ACCEPT"/g' /etc/default/ufw
+
+# Create a startup script to update iptables by means of cron job because they
+# will be reset on reboot
+mkdir -p "${HOME}/.scripts"
+cp helpers/cp/startup "${HOME}/.scripts/startup"
+
+# Add cron job for root user because we don't want to enter password for this :)
+sudo -u root sh -c '(crontab -l 2>/dev/null; echo "@reboot /bin/sh ${HOME}/.scripts/startup") | crontab -'
+
+# Restart docker and ufw services
+sudo systemctl restart ufw docker
